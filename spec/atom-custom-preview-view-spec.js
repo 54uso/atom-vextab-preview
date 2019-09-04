dfc9e6d07ffedb220804aed632ce23f9cd9fe639
彭川江 <pengchuanjiang@xiaomi.com>
2019-09-04 17:06:25 +0800

    first commit

---
+++ spec/atom-custom-preview-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomCustomPreviewView from '../lib/atom-custom-preview-view';
+
+describe('AtomCustomPreviewView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
